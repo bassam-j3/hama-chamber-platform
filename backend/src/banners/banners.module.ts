2dcee710a9fd2542e75d398a7fe076f96ed98377
bassam-j3 <bassam20047jamm@gmail.com>
2026-04-06 20:05:47 +0300

    fix: inject CloudinaryModule into BannersModule

--- backend/src/banners/banners.module.ts
+++ backend/src/banners/banners.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BannersService } from './banners.service';
 import { BannersController } from './banners.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module'; // 👈 استيراد موديل كلاوديناري
 
 @Module({
+  imports: [
+    PrismaModule, 
+    CloudinaryModule // 👈 إضافته هنا ليصبح متاحاً للكونترولر والسيرفس
+  ],
   controllers: [BannersController],
   providers: [BannersService],
 })
